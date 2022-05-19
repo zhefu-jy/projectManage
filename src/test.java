c34c5351addf060773ca226d51c32918ecb6af9e
zhefu-jy <84254306+zhefu-jy@users.noreply.github.com>
2022-05-19 20:31:50 +0800

    Update test.java
    模拟代码冲突

--- src/test.java
+++ src/test.java
@@ -1,5 +1,8 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("测试本地仓库提交");
+        System.out.println("模拟冲突net");
+        System.out.println("模拟冲突net");
+         
     }
 }
